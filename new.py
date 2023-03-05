8088185d40fc14d2442fbf94e99f86ddbc0b4df8
Astrogir1 <nastya.abashkina@mail.ru>
2023-03-05 23:04:33 +0300

    Initial Commit

--- new.py
+++ new.py
@@ -1,7 +1,18 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = int(input('Введите сумму вклада'))
-deposit = []
-deposit = [5600, 5900, 4280, 4000]
-list1=[0, 1, 2, 3]
-max_number = max(list1)
-print("Максимальная сумма, которую вы можете заработать: ", max(deposit))
+all_tickets = int(input('Введите количество билетов: '))
+numbers_ages = []
+for i in range(0, all_tickets):
+    input_value = int(input(f'Введите возраст участника №{i + 1}:\n'))
+    numbers_ages.append(input_value)
+    def price(age):
+        if age < 18:
+            return 0
+        elif 18 <= age < 25:
+            return 990
+        else:
+            return 1390
+    full_price = sum(map(price, numbers_ages))
+discount_price = int(full_price * 0.9)
+if all_tickets > 3:
+    print('Стоимость всех билетов со скидкой: ', discount_price, "руб.")
+else:
+    print('Стоимость всех билетов: ', full_price, "руб.")
